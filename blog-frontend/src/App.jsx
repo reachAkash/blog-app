b920c8a7e7b25adde60ad2ed36df0d628766fa86
VaishnavAkash <akashgupta9062@gmail.com>
2024-05-20 19:00:46 +0530

    update : created project

---
+++ blog-frontend/src/App.jsx
@@ -0,0 +1,5 @@
+function App() {
+  return <div className="text-red-500 container">Akash</div>;
+}
+
+export default App;
---
+++ blog-frontend/tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+        sm: "1.5rem",
+        md: "3rem",
+        lg: "5rem",
+        xl: "7.5rem",
+      },
+    },
+    screens: {
+      xxs: "300px",
+      xs: "475px",
+      sm: "639px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1281px",
+      "2xl": "1441px",
+      "3xl": "1920px",
+    },
+    extend: {
+      fontFamily: {
+        montserrat: ["Montserrat", "sans-serif"],
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
