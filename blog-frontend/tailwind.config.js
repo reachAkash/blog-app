fe8b142ec9ad0c66f4ee91b7d62afd79cc279017
VaishnavAkash <akashgupta9062@gmail.com>
2024-05-20 19:53:11 +0530

    update : navbar

--- blog-frontend/tailwind.config.js
+++ blog-frontend/tailwind.config.js
@@ -1,6 +1,8 @@
+import flowbite from "flowbite-react/tailwind";
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", flowbite.content()],
   theme: {
     container: {
       center: true,
@@ -29,5 +31,5 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [flowbite.plugin()],
 };
