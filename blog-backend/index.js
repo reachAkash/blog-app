c7fb23e8b62e9d8d05cce27e65090a1266b7ca3d
VaishnavAkash <akashgupta9062@gmail.com>
2024-05-20 20:20:32 +0530

    update : database connected.

--- blog-backend/index.js
+++ blog-backend/index.js
@@ -1,7 +1,13 @@
 const express = require("express");
-const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
-// app.get('/',())
+dotenv.config();
+const app = express();
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("Database Connected"))
+  .catch((err) => console.log(err));
 
 app.listen(3000, () => {
   console.log("Server Started!");
