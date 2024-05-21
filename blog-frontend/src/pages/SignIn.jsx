8ba7eebdcffaa2132b56a29b080eab8dce912216
VaishnavAkash <akashgupta9062@gmail.com>
2024-05-21 13:28:05 +0530

    update : google Auth

--- blog-frontend/src/pages/SignIn.jsx
+++ blog-frontend/src/pages/SignIn.jsx
@@ -3,6 +3,7 @@ import React, { useContext, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 import { Context } from "../ContextProvider";
+import OAuth from "../components/OAuth";
 
 const SignIn = () => {
   const navigate = useNavigate();
@@ -87,6 +88,7 @@ const SignIn = () => {
                 "Sign In"
               )}
             </Button>
+            <OAuth/>
           </form>
           <div className="flex gap-2 text-sm mt-5">
             <span>Don't have an account?</span>
--- blog-frontend/src/pages/SignUp.jsx
+++ blog-frontend/src/pages/SignUp.jsx
@@ -2,6 +2,7 @@ import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
+import OAuth from "../components/OAuth";
 
 const SignUp = () => {
   const navigate = useNavigate();
@@ -93,6 +94,7 @@ const SignUp = () => {
                 "Sign up"
               )}
             </Button>
+            <OAuth />
           </form>
           <div className="flex gap-2 text-sm mt-5">
             <span>Have an account?</span>
