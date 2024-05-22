9e4903758f89ea0bb0f3a94e20d0ae5edd851f57
VaishnavAkash <akashgupta9062@gmail.com>
2024-05-22 12:54:51 +0530

    update : dash posts

--- blog-frontend/src/pages/Dashboard.jsx
+++ blog-frontend/src/pages/Dashboard.jsx
@@ -1,29 +1,33 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import DashSidebar from "../components/DashSidebar";
 import DashProfile from "../components/DashProfile";
+import DashPosts from "../components/DashPosts";
+// import DashUsers from "../components/DashUsers";
+// import DashComments from "../components/DashComments";
+// import DashboardComp from "../components/DashboardComp";
 
-const Dashboard = () => {
+export default function Dashboard() {
   const location = useLocation();
   const [tab, setTab] = useState("");
-
   useEffect(() => {
     const urlParams = new URLSearchParams(location.search);
     const tabFromUrl = urlParams.get("tab");
-    console.log(tabFromUrl);
     if (tabFromUrl) {
       setTab(tabFromUrl);
     }
   }, [location.search]);
-
   return (
     <div className="min-h-screen flex flex-col md:flex-row">
       <div className="md:w-56">
         <DashSidebar />
       </div>
-      {tab == "profile" && <DashProfile />}
+
+      {tab === "profile" && <DashProfile />}
+      {tab === "posts" && <DashPosts />}
+      {/* {tab === "users" && <DashUsers />}
+      {tab === "comments" && <DashComments />}
+      {tab === "dash" && <DashboardComp />} */}
     </div>
   );
-};
-
-export default Dashboard;
+}
