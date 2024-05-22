5f3c45d88cf7c75c92c2999598131df79ace2407
VaishnavAkash <akashgupta9062@gmail.com>
2024-05-22 13:18:21 +0530

    update : get Posts api.

--- blog-backend/routes/post.route.js
+++ blog-backend/routes/post.route.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const { verifyToken } = require("../utils/verifyUser");
-const { create } = require("../controllers/post.controller.js");
+const { create, getposts } = require("../controllers/post.controller.js");
 
 const router = express.Router();
 
 router.post("/create", verifyToken, create);
+router.get("/getposts", getposts);
 
 module.exports = router;
