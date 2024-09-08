57359f3e36ae3a50109dde3bf8518ba37ef55653
Akash <114148533+reachAkash@users.noreply.github.com>
2024-09-08 11:58:10 +0530

    update : added loader

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -22,7 +22,7 @@ const Home = () => {
   }, []);
 
   return (
-    <div>
+ <div>
       <div className="flex flex-col gap-6 p-28 px-3 max-w-7xl mx-auto">
         <h1 className="text-3xl font-bold lg:text-6xl">
           Welcome to <span className="text-purple-500">Akash's</span> Blogs.
@@ -37,7 +37,7 @@ const Home = () => {
         ></Link>
       </div>
       <div className="max-w-6xl mx-auto p-3 flex flex-col gap-8 py-7">
-        {posts && posts.length > 0 && (
+        {posts && posts.length > 0 ? (
           <div className="flex flex-col gap-6">
             <h2 className="text-2xl font-semibold text-center">Recent Posts</h2>
             <div className="flex flex-wrap gap-4">
@@ -52,7 +52,7 @@ const Home = () => {
               View all posts
             </Link>
           </div>
-        )}
+        ) : <div className="flex items-center justify-center m-auto text-lg font-semibold">Loading ...</div>}
       </div>
     </div>
   );
